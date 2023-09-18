c5c90e0ada097f890db0da7b388effd38b4eaad3
Timur Lagawy <timurtagirovich@mail.ru>
2023-09-18 21:09:30 +0200

    Convert a String to a Number!

--- script.js
+++ script.js
@@ -90,4 +90,13 @@ function numberToString(num) {
   let a = String(num);
   return a;
   // Return a string of the number here!
+}/*Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.*/
+const stringToNumber = function(str){
+  
+  return Number(str);
 }
