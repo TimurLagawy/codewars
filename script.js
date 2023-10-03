466415d9bc36678875a4411c964caaa0c19aae3e
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-03 20:41:00 +0200

    feat: Simple multiplication

--- script.js
+++ script.js
@@ -215,3 +215,11 @@ function solution(str, ending) {
     return false;
   }
 }
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.*/
+function simpleMultiplication(number) {
+  if (number % 2 == 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+}
