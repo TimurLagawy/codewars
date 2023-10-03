866690db33f9a47b1e1a8309c522512b1220e61d
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-03 19:53:24 +0200

    feat: Remove First and Last Character

--- script.js
+++ script.js
@@ -171,6 +171,14 @@ blocks
 The blocksize is a by a and a ≥ 1.
 
 What is the perimeter of the nth shape in the sequence (n ≥ 1) ?*/
-function perimeterSequence(a,n) {
+function perimeterSequence(a, n) {
   return 4 * a * n;
+}
+/*It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.*/
+function removeChar(str) {
+  let newStr = "";
+  for (let i = 1; i < str.length - 1; i++) {
+    newStr = newStr + str[i];
+  }
+  return newStr;
 }
