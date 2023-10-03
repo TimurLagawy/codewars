2c3e4b87acff776a6d4d06c812ba74f871d9a687
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-03 19:29:04 +0200

    feat: Number of Decimal Digits

--- script.js
+++ script.js
@@ -147,3 +147,10 @@ function areaLargestSquare(r) {
   let s = r * r * 2;
   return s; //code here
 }
+/*Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.*/
+function digits(n) {
+  
+  return n.toString().length;
+}
