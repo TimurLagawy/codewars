c98e3233e1256d9cae3c7a9ce9fba84ffe4a700d
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-03 20:26:58 +0200

    feat:String ends with?

--- script.js
+++ script.js
@@ -200,3 +200,14 @@ function noSpace(x) {
   }
   return newX;
 }
+/*Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false*/
+function solution(str, ending) {
+  if (ending[ending.length - 1] == str[str.length - 1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
