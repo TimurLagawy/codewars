b517aaae432e9ef53e5e43c7f7b09ea49707014b
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-04 08:56:49 +0200

    feat: Thinkful - Logic Drills: Traffic light

--- script.js
+++ script.js
@@ -269,7 +269,9 @@ function finalGrade(exam, projects) {
 
 Input :: 1
 
-Output :: "One".*/
+Output :: "One". function switchItUp(n){
+  return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+}*/
 function switchItUp(n) {
   if (n == 1) {
     return "One";
@@ -291,5 +293,19 @@ function switchItUp(n) {
     return "Nine";
   } else {
     return "Zero";
+  }
+}
+/*You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.*/
+function updateLight(current) {
+  if (current == "green") {
+    return "yellow";
+  } else if (current == "yellow") {
+    return "red";
+  } else if (current == "red") {
+    return "green";
   }
 }
