fbc77b761f5ac199edffafd2bf149af22ecaa5da
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-04 08:49:44 +0200

    feat: Switch it Up!

--- script.js
+++ script.js
@@ -265,3 +265,31 @@ function finalGrade(exam, projects) {
   else if (exam > 50 && projects >= 2) return 75;
   else return 0;
 }
+/*When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".*/
+function switchItUp(n) {
+  if (n == 1) {
+    return "One";
+  } else if (n == 2) {
+    return "Two";
+  } else if (n == 3) {
+    return "Three";
+  } else if (n == 4) {
+    return "Four";
+  } else if (n == 5) {
+    return "Five";
+  } else if (n == 6) {
+    return "Six";
+  } else if (n == 7) {
+    return "Seven";
+  } else if (n == 8) {
+    return "Eight";
+  } else if (n == 9) {
+    return "Nine";
+  } else {
+    return "Zero";
+  }
+}
