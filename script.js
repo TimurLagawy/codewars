0267b6a9a8941d4b4a2352157396d6735ceeedd7
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-04 10:08:51 +0200

    feat: Type of sum

--- script.js
+++ script.js
@@ -326,7 +326,10 @@ You will need a rental car in order for you to get around in your vacation. The 
 
 Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
-Write a code that gives out the total amount for different days(d).*/
+Write a code that gives out the total amount for different days(d).
+function rentalCarCost(d) {
+  return d * 40 - (d >= 7 ? 50 : (d >= 3 ? 20 : 0));
+}*/
 function rentalCarCost(d) {
   if (d < 3) {
     return 40 * d;
@@ -335,4 +338,8 @@ function rentalCarCost(d) {
   } else {
     return 40 * d - 50;
   }
+}
+/*Return the type of the sum of the two arguments*/
+function typeOfSum(a, b) {
+  return typeof(a + b);
 }
