f1ba6a9e4c794c43f7215192603dcaa17d2e836d
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-08 08:36:36 +0200

    feat: concatenateStrings

--- script.js
+++ script.js
@@ -474,3 +474,18 @@ function colourAssociation(array) {
   }
   return arr;
 }
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
+}
