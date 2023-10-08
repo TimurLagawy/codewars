c40c1f384211cfe3798293758cda5468b2ebcdd3
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-08 19:55:42 +0200

    feat: getStringFromTemplate

--- script.js
+++ script.js
@@ -503,3 +503,24 @@ function concatenateStrings(value1, value2) {
 function getStringLength(value) {
   return value.length;
 }
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+  let res = "Hello, ";
+  res += firstName;
+  res += " ";
+  res += lastName;
+  res += "!";
+  return res;
+}
