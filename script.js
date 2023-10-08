08ace9a7cd2f9f2f58ae76ab73d553204b1d3892
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-08 19:54:16 +0200

    feat: getStringLength

--- script.js
+++ script.js
@@ -489,3 +489,17 @@ function colourAssociation(array) {
 function concatenateStrings(value1, value2) {
   return value1 + value2;
 }
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+  return value.length;
+}
