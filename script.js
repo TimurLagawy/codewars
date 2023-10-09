30baa502bed8c8ca003cc7f5befebb0017645d08
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-09 18:26:41 +0200

    feat: extractEmails

--- script.js
+++ script.js
@@ -655,3 +655,22 @@ function convertToUpperCase(str) {
   }
   return res;
 }
+/**
+ * Extracts e-mails from single string with e-mails list delimeted by semicolons
+ *
+ * @param {string} str
+ * @return {array}
+ *
+ * @example
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
+ *   => [
+ *      'angus.young@gmail.com',
+ *      'brian.johnson@hotmail.com',
+ *      'bon.scott@yahoo.com'
+ *   ],
+ *   'info@gmail.com' => ['info@gmail.com']
+ */
+function extractEmails(str) {
+  const array = str.split(';');
+  return array;
+}
