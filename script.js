1a03efdad0b51da74dc12e932f19484fffdb7531
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-10 17:09:00 +0200

    feat: getCircleCircumference

--- script.js
+++ script.js
@@ -869,3 +869,17 @@ function getCardId(value) {
 function getRectangleArea(width, height) {
   return width * height;
 }
+/**
+ * Returns a circumference of circle given by radius.
+ *
+ * @param {number} radius
+ * @return {number}
+ *
+ * @example:
+ *   5    => 31.41592653589793
+ *   3.14 => 19.729201864543903
+ *   0    => 0
+ */
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
