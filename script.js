bbbcad4b2f8ded703320e390dcc4835c1f7f51ff
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-10 14:21:12 +0200

    feat: encodeToRot13

--- script.js
+++ script.js
@@ -713,3 +713,36 @@ function getRectangleString(width, height) {
   str += "┘\n";
   return str;
 }
+/**
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' =>
+ *   'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+function encodeToRot13(str) {
+  const original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const remove = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  let res = "";
+
+  for (let j = 0; j < str.length; j += 1) {
+    if (str[j] === " " || str[j] === "?" || str[j] === "!") {
+      res += str[j];
+    }
+    for (let i = 0; i < original.length; i += 1) {
+      if (str[j] === original[i]) {
+        res += remove[i];
+      }
+    }
+  }
+  return res;
+}
