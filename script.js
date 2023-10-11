34e6a2f29e2187b37fac9f7854387d074dbd0412
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-11 20:33:50 +0200

    feat: getLinearEquationRoot

--- script.js
+++ script.js
@@ -917,3 +917,18 @@ function getAverage(value1, value2) {
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
   return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
+/**
+ * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *
+ * @example:
+ *   5*x - 10 = 0    => 2
+ *   x + 8 = 0       => -8
+ *   5*x = 0         => 0
+ */
+function getLinearEquationRoot(a, b) {
+  return -b / a;
+}
