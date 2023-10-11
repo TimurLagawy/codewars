380bb8dad9d4e5c1bdfe808ec5bbc31b920a82d9
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-11 20:33:04 +0200

    feat: getDistanceBetweenPoints

--- script.js
+++ script.js
@@ -899,3 +899,21 @@ function getAverage(value1, value2) {
   const res = value1 / 2 + value2 / 2;
   return res;
 }
+/**
+ * Returns a distance between two points by cartesian coordinates.
+ *
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ *
+ * @return {number}
+ *
+ * @example:
+ *   (0,0) (0,1)    => 1
+ *   (0,0) (1,0)    => 1
+ *   (-5,0) (10,-10) => 18.027756377319946
+ */
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
