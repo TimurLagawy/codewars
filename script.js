66856bc8433c1c09d04cf33e3056c1217fa50470
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-16 19:51:50 +0200

    feat: findElement

--- script.js
+++ script.js
@@ -1071,3 +1071,18 @@ function toNumber(value, def) {
   }
   return res1;
 }
+/**
+ * Returns an index of the specified element in array or -1 if element is not found
+ *
+ * @param {array} arr
+ * @param {any} value
+ * @return {number}
+ *
+ * @example
+ *    ['Ace', 10, true], 10    => 1
+ *    ['Array', 'Number', 'string'], 'Date'    => -1
+ *    [0, 1, 2, 3, 4, 5], 5    => 5
+ */
+function findElement(arr, value) {
+  return arr.includes(value, 0) ? arr.indexOf(value) : -1;
+}
