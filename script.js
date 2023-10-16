3bfaf8cb953c9740ca3dc2689f300551c307c4f5
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-16 19:49:18 +0200

    feat: parseNumberFromString & roundToPowerOfTen

--- script.js
+++ script.js
@@ -991,3 +991,28 @@ function getLastDigit(value) {
 function parseNumberFromString(value) {
   return Number(value);
 }
+/**
+ * Returns the number rounded to specified power of 10.
+ *
+ * @param {number} num
+ * @param {number} pow
+ * @return {number}
+ *
+ * @example:
+ *   1234, 0  => 1234
+ *   1234, 1  => 1230
+ *   1234, 2  => 1200
+ *   1234, 3  => 1000
+ *   1678, 0  => 1678
+ *   1678, 1  => 1680
+ *   1678, 2  => 1700
+ *   1678, 3  => 2000
+ */
+function roundToPowerOfTen(num, pow) {
+  let res = "1";
+  for (let i = 0; i < pow; i += 1) {
+    res += 0;
+  }
+  res = Number(res);
+  return Math.round(num / res) * res;
+}
