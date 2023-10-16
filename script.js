7728c2beacc0e2a0271241c6a0455730ae218f5c
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-16 19:50:40 +0200

    feat: toNumber

--- script.js
+++ script.js
@@ -1045,3 +1045,29 @@ function isPrime(n) {
   }
   return true;
 }
+/**
+ * Tries to convert value to number and returns it if conversion was successful;
+ * otherwise returns default value passed as a second argument.
+ *
+ * @param {any} value
+ * @param {any} def
+ * @return {number}
+ *
+ * @example
+ *   toNumber(null, 0) => 0
+ *   toNumber('test', 0) => 0
+ *   toNumber('1', 0) => 1
+ *   toNumber(42, 0) => 42
+ *   toNumber(new Number(42), 0) => 42
+ */
+function toNumber(value, def) {
+  const res = Number(value);
+  let res1;
+  // eslint-disable-next-line no-restricted-globals
+  if (!isNaN(res) || value instanceof Number) {
+    res1 = res;
+  } else {
+    res1 = def;
+  }
+  return res1;
+}
