cd2591ce0bb4858e66088505b41748d9ace9588f
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-17 12:24:37 +0200

    feat: towelSort

--- script.js
+++ script.js
@@ -1248,3 +1248,19 @@ function reverse(n) {
   let str1 = str.split("").reverse().join("");
   return Number(str1);
 }
+
+function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) {
+      matrix[i].reverse();
+    }
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr.push(matrix[i][j]);
+    }
+  }
+  return arr;
+}
