1c3f85344749e159e630b0d157f7516f9867e07c
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-18 12:46:00 +0200

    feat: GuessingGame

--- script.js
+++ script.js
@@ -1283,3 +1283,28 @@ function check(str, bracketsConfig) {
     return false;
   }
 }
+class GuessingGame {
+  constructor() {
+    this.minRange = 0;
+    this.maxRange = 0;
+    this.guessValue = 0;
+  }
+
+  setRange(min, max) {
+    this.minRange = min;
+    this.maxRange = max;
+  }
+
+  guess() {
+    this.guessValue = Math.round((this.minRange + this.maxRange) / 2);
+    return this.guessValue;
+  }
+
+  lower() {
+    this.maxRange = this.guessValue;
+  }
+
+  greater() {
+    this.minRange = this.guessValue;
+  }
+}
