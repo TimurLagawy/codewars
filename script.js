0c0beee399897582e9ad0d99e0c46e98c3a15876
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-18 18:45:22 +0200

    feat: getArrayOfStrings

--- script.js
+++ script.js
@@ -1308,3 +1308,18 @@ class GuessingGame {
     this.minRange = this.guessValue;
   }
 }
+/** ***********************************codewars
+ * Returns the array with strings only in the specified array (in original order)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 'cat', 3, true, 'dog' ] => [ 'cat', 'dog' ]
+ *    [ 1, 2, 3, 4, 5 ] => []
+ *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
+ */
+function getArrayOfStrings(arr) {
+  const results = arr.filter((item) => typeof item === "string");
+  return results;
+}
