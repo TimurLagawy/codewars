2f41b013180c0c81f91f13798587e8c84a33661a
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-19 21:11:26 +0200

    feat:getUpperCaseStrings

--- script.js
+++ script.js
@@ -1364,3 +1364,16 @@ function removeFalsyValues(arr) {
 function getUpperCaseStrings(arr) {
   return arr.map((item) => item.toUpperCase());
 }
+/**
+ * Returns the array of string lengths from the specified string array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
+ *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
+ */
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
+}
