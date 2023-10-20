053b3b22f2a93e8897369a68653e8a8d368c3f40
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-20 16:54:06 +0200

    feat: getMovingSum

--- script.js
+++ script.js
@@ -1457,3 +1457,25 @@ function toCsvText(arr) {
 function toArrayOfSquares(arr) {
   return arr.map((item) => item * item);
 }
+
+/**
+ * Transforms the numeric array to the according moving sum array:
+ *     f[n] = x[0] + x[1] + x[2] +...+ x[n]
+ *  or f[n] = f[n-1] + x[n]
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * Example :
+ *   [ 1, 1, 1, 1, 1 ]        => [ 1, 2, 3, 4, 5 ]
+ *   [ 10, -10, 10, -10, 10 ] => [ 10, 0, 10, 0, 10 ]
+ *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
+ *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
+ */
+function getMovingSum(arr) {
+  let sum = 0;
+  return arr.map((item) => {
+    sum += item;
+    return sum;
+  });
+}
