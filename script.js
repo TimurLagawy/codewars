a6577750c2833012e5545cb89848ad8db418a7ae
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-20 16:54:48 +0200

    feat: getSecondItems

--- script.js
+++ script.js
@@ -1479,3 +1479,17 @@ function getMovingSum(arr) {
     return sum;
   });
 }
+/**
+ * Returns every second item from the specified array:
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * Example :
+ * [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 2, 4, 6, 8, 10 ]
+ * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
+ * [ "a" ] => []
+ */
+function getSecondItems(arr) {
+  return arr.filter((_, index) => index % 2 === 1);
+}
