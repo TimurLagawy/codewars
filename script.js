70ca570e776a4992189fb1a11c262450a4feb5e3
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-20 16:52:57 +0200

    feat: toCsvText

--- script.js
+++ script.js
@@ -1418,3 +1418,27 @@ function getTail(arr, n) {
   return arr.slice(-n);
   // return arr.splice(0, arr.length - n);
 }
+
+/**
+ * Returns CSV representation of two-dimensional numeric array.
+ * https://en.wikipedia.org/wiki/Comma-separated_values
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [
+ *       [  0, 1, 2, 3, 4 ],
+ *       [ 10,11,12,13,14 ],
+ *       [ 20,21,22,23,24 ],
+ *       [ 30,31,32,33,34 ]
+ *    ]
+ *           =>
+ *     '0,1,2,3,4\n'
+ *    +'10,11,12,13,14\n'
+ *    +'20,21,22,23,24\n'
+ *    +'30,31,32,33,34'
+ */
+function toCsvText(arr) {
+  return arr.join("\n");
+}
