8b23a61013eee4dfc61754854989b8866a1423bb
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-21 08:48:46 +0200

    feat: group

--- script.js
+++ script.js
@@ -1755,3 +1755,44 @@ function distinct(arr) {
   return results; */
   return arr.filter((item, index) => arr.indexOf(item) === index);
 }
+/**
+ * Groups elements of the specified array by key.
+ * Returns multimap of keys extracted from array elements via keySelector callback
+ * and values extracted via valueSelector callback.
+ * See: https://en.wikipedia.org/wiki/Multimap
+ *
+ * @param {array} array
+ * @param {Function} keySelector
+ * @param {Function} valueSelector
+ * @return {Map}
+ *
+ * @example
+ *   group([
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Russia', city: 'Omsk' },
+ *      { country: 'Russia', city: 'Samara' },
+ *      { country: 'Belarus', city: 'Grodno' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland', city: 'Lodz' }
+ *     ],
+ *     item => item.country,
+ *     item => item.city
+ *   )
+ *            =>
+ *   Map {
+ *    "Belarus" => ["Brest", "Grodno", "Minsk"],
+ *    "Russia" => ["Omsk", "Samara"],
+ *    "Poland" => ["Lodz"]
+ *   }
+ */
+function group(array, keySelector, valueSelector) {
+  return array.reduce((resultMap, item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+
+    const valuesArray = resultMap.get(key) || [];
+    resultMap.set(key, [...valuesArray, value]);
+
+    return resultMap;
+  }, new Map());
+}
