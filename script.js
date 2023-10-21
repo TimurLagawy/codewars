ed645aff6be80f619589283243aa0cf84dab0395
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-21 08:38:00 +0200

    feat: get3TopItems

--- script.js
+++ script.js
@@ -1515,3 +1515,21 @@ function propagateItemsByPositionIndex(arr) {
   return Array.from(arr, (item, index) => (index + 1) * item); */
   return arr.flatMap((item, index) => Array(index + 1).fill(item));
 }
+/** ****************************************************codewars
+ * Returns the 3 largest numbers from the specified array
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [] => []
+ *   [ 1, 2 ] => [ 2, 1 ]
+ *   [ 1, 2, 3 ] => [ 3, 2, 1 ]
+ *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
+ *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
+ */
+function get3TopItems(arr) {
+  arr.reverse();
+
+  return arr.slice(0, 3);
+}
