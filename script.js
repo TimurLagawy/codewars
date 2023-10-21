22e239f83c1b4d42785d6ea83688441f7563acc9
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-21 08:43:29 +0200

    feat: toStringList

--- script.js
+++ script.js
@@ -1646,3 +1646,17 @@ function findAllOccurrences(arr, n) {
   const res = arr.filter((item) => item === n);
   return res.length;
 }
+/**
+ * Concatenates all elements from specified array into single string with ',' delimiter
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [0, false, 'cat', NaN, true, '']  => '0,false,cat,NaN,true,'
+ *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
+ *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
+ */
+function toStringList(arr) {
+  return arr.join(',');
+}
