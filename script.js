6edcc0a65396af1d4de367623c2ea1d47cbf35ec
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-25 20:22:45 +0200

    feat: task 'Who is the killer'

--- script.js
+++ script.js
@@ -2391,3 +2391,23 @@ function dataReverse(data) {
   }
   return mass1;
 }
+/* Who is the killer?
+Some people have been killed!
+You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+Task.
+Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+{'James': ['Jacob', 'Bill', 'Lucas'],
+ 'Johnny': ['David', 'Kyle', 'Lucas'],
+ 'Peter': ['Lucy', 'Kyle']}
+and also a list of the names of the dead people:
+
+['Lucas', 'Bill'] */
+function killer(suspectInfo, dead) {
+  for (const [suspect, seen] of Object.entries(suspectInfo)) {
+    if (dead.every(deads => seen.includes(deads))) {
+      return suspect;
+    }
+  }
+}
