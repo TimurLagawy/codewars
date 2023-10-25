aa5bc7307eefdce79db805c05f4006a0e5d05b96
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-25 20:58:30 +0200

    feat: add vowelOne

--- script.js
+++ script.js
@@ -2419,8 +2419,31 @@ for (const key in obj){
 } */
 function killer(suspectInfo, dead) {
   for (const [suspect, seen] of Object.entries(suspectInfo)) {
-    if (dead.every(deads => seen.includes(deads))) {
+    if (dead.every((deads) => seen.includes(deads))) {
       return suspect;
     }
   }
+}
+/* vowelOne
+Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+Examples:
+
+vowelOne( "abceios" ) // "1001110"
+
+vowelOne( "aeiou, abc" ) // "1111100100"
+ */
+function vowelOne(s) {
+  let vov = "aeiou";
+  let res = "";
+  for (let i = 0; i < s.length; i++) {
+    if (vov.includes(s[i].toLowerCase())) {
+      res += 1;
+    } else {
+      res += 0;
+    }
+  }
+  return res;
 }
