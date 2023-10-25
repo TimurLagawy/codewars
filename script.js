4c9b8bc653f7b0cb258efcc29ac23dc2e630178c
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-25 20:27:28 +0200

    feat: add new killer

--- script.js
+++ script.js
@@ -2404,6 +2404,19 @@ Given a dictionary with all the names of the suspects and everyone that they hav
 and also a list of the names of the dead people:
 
 ['Lucas', 'Bill'] */
+/* function killer(obj, arr) {
+for (const key in obj){
+  let same = 0;
+  for(let i = 0;i<arr.length;i++){
+    if(obj[key].includes(arr[i])){
+      same++
+    }
+     if(same === arr.length){
+       return key
+     }
+  }
+}
+} */
 function killer(suspectInfo, dead) {
   for (const [suspect, seen] of Object.entries(suspectInfo)) {
     if (dead.every(deads => seen.includes(deads))) {
