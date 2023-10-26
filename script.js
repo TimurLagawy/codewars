4c223005b68cab87d75674e7c458b7237c9a8584
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-26 19:36:41 +0200

    feat: add maxTriSum

--- script.js
+++ script.js
@@ -2655,3 +2655,54 @@ function nicknameGenerator(name) {
   }
   return nick;
 }
+
+/* DESCRIPTION:
+Task
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+Explanation:
+As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+Explanation:
+As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+Explanation:
+As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once . */
+function maxTriSum(numbers) {
+  let mass1 = numbers.sort((a, b) => b - a);
+  let count = 0;
+  let res = 0;
+  for (let i = 0; i < mass1.length; i++) {
+    if (mass1[i] !== mass1[i + 1]) {
+      count++;
+      res += mass1[i];
+      if (count == 3) {
+        break;
+      }
+    }
+  }
+  console.log(res);
+  return res;
+}
+/*et ob1 = { algebra: 6, history: 7, physics: 8, geography: 9, chemistry: 10 };
+let ob2 = { algebra: 8, history: 7, physics: 8, geography: 9, chemistry: 10 };
+let ob3 = { algebra: 6, history: 5, physics: 5, geography: 9, chemistry: 10 };
+console.log(Object.values(ob1));
+*/
