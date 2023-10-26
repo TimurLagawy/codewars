088dc3b99a9db8e7ca66b7f2db5efb106df2220d
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-26 19:52:30 +0200

    feat: add findMissing

--- script.js
+++ script.js
@@ -2716,6 +2716,22 @@ getDecimal(-0.2) === 0.2 */
 function getDecimal(n) {
   return Math.abs(n % 1);
 }
+/* Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, find the missing element.
+
+Please note, there may be duplicates in the arrays, so checking if a numerical value exists in one and not the other is not a valid solution.
+
+find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+The first array will always have at least one element.
+
+*/
+const findMissing = (arr1, arr2) => {
+  mass1 = arr1.sort();
+  mass2 = arr2.sort();
+  for(let i = 0; i < mass1.length; i++){
+    if(mass1[i] != mass2[i]) return mass1[i];
+  }
+}
 
 /*et ob1 = { algebra: 6, history: 7, physics: 8, geography: 9, chemistry: 10 };
 let ob2 = { algebra: 8, history: 7, physics: 8, geography: 9, chemistry: 10 };
