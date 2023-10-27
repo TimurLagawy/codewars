15cb08e15ee642ade014760ac4c6c80e7e8a39a8
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-27 11:37:22 +0200

    feat: task Whose bicycle?

--- script.js
+++ script.js
@@ -2818,3 +2818,50 @@ function combine(...objects) {
     return result;
   }, {});
 }
+
+/* I am the father of three wonderful sons. before the beginning of the school year, I promised them that I would buy a bicycle for someone who would bring the best marks at the end of the school year. it's time to keep promises and I count on you.
+
+You have 3 input objects(school diaries) with school subjects and marks (1-10). For example:
+
+{
+  'algebra': 6,
+  'history': 8,
+  'physics': 9,
+  'geography': 2,
+  'chemistry': 9
+}
+Return please :
+
+'I need to buy a bicycle for my first son.' // the sum of the marks is the highest  in the first diary.
+
+'I need to buy a bicycle for my second son.' // the sum of the marks is the highest in the second diary.
+
+'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
+If two or three sons have the same highest marks, you need to choose the younger one. Use the age table which is constant and preloaded:
+
+const ageTable = {
+                  'firstSonAge': 14,
+                  'secondSonAge': 9,
+                  'thirdSonAge': 8
+                } */
+function whoseBicycle(obj1, obj2, obj3) {
+  let res1 = 0;
+  let res2 = 0;
+  let res3 = 0;
+  for (let key in obj1) {
+    res1 += obj1[key];
+  }
+  for (let key in obj2) {
+    res2 += obj2[key];
+  }
+  for (let key in obj3) {
+    res3 += obj3[key];
+  }
+  if (res1 > res2 && res1 > res3) {
+    return "I need to buy a bicycle for my first son.";
+  } else if (res2 > res1 && res2 > res3) {
+    return "I need to buy a bicycle for my second son.";
+  } else {
+    return "I need to buy a bicycle for my third son.";
+  }
+}
