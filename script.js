1ed4c6af4cdb4af692faab0360f9f85281f435be
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-27 10:41:27 +0200

    feat: add task Combine objects

--- script.js
+++ script.js
@@ -2783,3 +2783,24 @@ function nbDig(n, d) {
   let filt = res1.filter((item) => item == d.toString()).length;
   return filt;
 }
+
+/* Your task is to write a function that takes two or more objects and returns a new object which combines all the input objects.
+
+All input object properties will have only numeric values. Objects are combined together so that the values of matching keys are added together.
+
+An example:
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
+The combine function should be a good citizen, so should not mutate the input objects. */
+function combine(...objects) {
+  return objects.reduce((result, obj) => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result[key] = (result[key] || 0) + obj[key];
+      }
+    }
+    return result;
+  }, {});
+}
