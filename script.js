b18ffa17acefd8bd2fe38194d7ab5b0ea70d77b7
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-27 09:27:05 +0200

    feat: add Count of digits

--- script.js
+++ script.js
@@ -2728,13 +2728,48 @@ The first array will always have at least one element.
 const findMissing = (arr1, arr2) => {
   mass1 = arr1.sort();
   mass2 = arr2.sort();
-  for(let i = 0; i < mass1.length; i++){
-    if(mass1[i] != mass2[i]) return mass1[i];
+  for (let i = 0; i < mass1.length; i++) {
+    if (mass1[i] != mass2[i]) return mass1[i];
   }
-}
+};
 
-/*et ob1 = { algebra: 6, history: 7, physics: 8, geography: 9, chemistry: 10 };
-let ob2 = { algebra: 8, history: 7, physics: 8, geography: 9, chemistry: 10 };
-let ob3 = { algebra: 6, history: 5, physics: 5, geography: 9, chemistry: 10 };
-console.log(Object.values(ob1));
-*/
+/* Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Implement the function taking n and d as parameters and returning this count.
+
+Examples:
+n = 10, d = 1 
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+The function, when given n = 25 and d = 1 as argument, should return 11 since
+the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25.
+Note that 121 has twice the digit 1. 
+
+function nbDig(n, d) {
+  let count = 0;
+
+  for (let i = 0; i <= n; i++) {
+    let square = i ** 2;
+    let digits = square.toString().split('');
+
+    count += digits.filter(item => item === d.toString()).length;
+  }
+
+  return count;
+} */
+function nbDig(n, d) {
+  let res = "";
+  for (let i = 0; i <= n; i++) {
+    res += i ** 2;
+  }
+  let res1 = res.split("");
+  let filt = res1.filter((item) => item == d.toString()).length;
+  return filt;
+}
