242ca7c04fb75cc5cafec5d7d195f4546ca42e38
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-29 15:38:04 +0100

    feat: add getDigitalRoot

--- script.js
+++ script.js
@@ -3037,3 +3037,35 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
   }
   return res;
 }
+
+/**
+ * Returns the digital root of integer:
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+function getDigitalRoot(num) {
+  const str = String(num);
+  let res = 0;
+  let res1 = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    res += Number(str[i]);
+  }
+  if (res < 10) {
+    res1 = res;
+  } else {
+    const str1 = String(res);
+    for (let i = 0; i < str1.length; i += 1) {
+      res1 += Number(str1[i]);
+    }
+  }
+  return res1;
+}
