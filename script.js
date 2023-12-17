4c261509b70bbd6c0686e9dfe648b91716b9751d
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-17 18:23:28 +0100

    feat: add removeLeadingAndTrailingWhitespaces function

--- script.js
+++ script.js
@@ -3494,3 +3494,18 @@ function isString(value) {
   return res; */
   return typeof value === 'string' || value instanceof String;
 }
+
+/**
+ * Removes leading and trailing whitespace characters from the string.
+ *
+ * @param {string} value - The input string to remove leading and trailing whitespaces from.
+ * @return {string} - The string with leading and trailing whitespaces removed.
+ *
+ * @example
+ *   removeLeadingAndTrailingWhitespaces('  Abracadabra') => 'Abracadabra'
+ *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
+ *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
+}
