d01c2c9ef6a38700da42ea577767578fec290b4b
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-17 14:01:54 +0100

    feat: add removeLeadingAndTrailingWhitespaces

--- script.js
+++ script.js
@@ -3456,3 +3456,18 @@ function getFirstChar(value) {
   }
   return res;
 }
+
+/**
+ * Removes leading and trailing whitespace characters from the string.
+ *
+ * @param {string} value - The input string to remove leading and trailing whitespaces from.
+ * @return {string} - The string with leading and trailing whitespaces removed.
+ *
+ * @example
+ *   removeLeadingAndTrailingWhitespaces('  Abracadabra') => 'Abracadabra'
+ *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
+ *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
+}
