5d1045d7ecd58e5d8ff08d4ad80e6a6d99dca1f7
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-18 19:57:18 +0100

    feat: add encodeToRot13 function

--- script.js
+++ script.js
@@ -3828,3 +3828,33 @@ function unbracketTag(str) {
 function extractEmails(str) {
   return str.split(';');
 }
+
+/**
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str - The input string.
+ * @return {string} - The ROT13 encoded string.
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+ *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+function encodeToRot13(str) {
+  const key1 = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?';
+  const key2 = ' NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm!?';
+  let res = '';
+  for (let i = 0; i < str.length; i += 1) {
+    for (let l = 0; l < key1.length; l += 1) {
+      if (str[i] === key1[l]) {
+        res += key2[l];
+      }
+    }
+  }
+  return res;
+}
