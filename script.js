660fd84bdb9c7d6c884d6f84e5ef3e61f9337664
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-25 19:10:16 +0100

    feat: add removeKFromList function

--- script.js
+++ script.js
@@ -4073,3 +4073,65 @@ function getFloatOnString(str) {
   return res; */
   return parseFloat(str);
 }
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Given a singly linked list of integers l and an integer k,
+ * remove all elements from list l that have a value equal to k.
+ *
+ * @param {List} l
+ * @param {Number} k
+ * @return {List}
+ *
+ * @example
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * the output should be [1, 2, 4, 5]
+ *
+ * Singly - linked lists are already defined using interface
+ * class ListNode {
+ *   constructor(x) {
+ *     this.value = x;
+ *     this.next = null;
+ *   }
+ * }
+ */
+/*
+function removeKFromList(l, k) {
+  let res = [];
+  for (let i = 0; i < l.length; i++) {
+    if (k !== l[i]) {
+      res.push(l[i]);
+    }
+  }
+  return res;
+} */
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
+function removeKFromList(l, k) {
+  // Handle the case where the list is empty
+  if (!l) {
+    return null;
+  }
+
+  // Create a dummy node to simplify edge cases
+  const dummy = new ListNode(0);
+  dummy.next = l;
+
+  let current = dummy;
+
+  // Traverse the list and remove nodes with value equal to k
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return dummy.next;
+}
