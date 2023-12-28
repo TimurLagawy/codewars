22b89e2e010b6d8ffd3d5bca6e3fd815df56c571
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-28 19:47:57 +0100

    feat: add countCats function

--- script.js
+++ script.js
@@ -4170,3 +4170,24 @@ function getCharacterCount(s) {
 
   return charCount;
 }
+
+
+/**
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix
+ * @return {Number} count of cats found
+ *
+ * @example
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
+ *
+ */
+function countCats(arr) {
+  let k = 0;
+  arr.forEach((a) => a.forEach((b) => (b == "^^" ? k++ : k)));
+  return k;
+}
